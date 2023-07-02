dfe5294cb6d4e5349eb6193c1c778c5630b5de6d
Goran Knezevic <goran.knezevic@kolotree.com>
2023-07-02 16:08:34 +0200

    First failing test

--- src/test/java/com/example/springplayground/SpringPlaygroundApplicationTests.java
+++ src/test/java/com/example/springplayground/SpringPlaygroundApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.springplayground;
 
+import com.example.springplayground.adapter.DatabaseAdapter;
+import com.example.springplayground.model.Book;
+import com.example.springplayground.model.Genre;
+import com.example.springplayground.port.incoming.BooksPort;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringPlaygroundApplicationTests {
 
+    private BooksPort booksPort;
+
+    @BeforeEach
+    public void beforeEach() {
+        booksPort = new DatabaseAdapter();
+    }
+
     @Test
-    void contextLoads() {
+    public void shouldReturnedStoredBookUsingIdTest() {
+        Book book = Book.create("My Test book", Genre.COMEDY);
+        Book savedBook = booksPort.save(book);
+        Book fetchedBook = booksPort.get(savedBook.getId());
+        assertNotNull(fetchedBook);
+        assertEquals(savedBook.getId(), fetchedBook.getId());
     }
 
 }
