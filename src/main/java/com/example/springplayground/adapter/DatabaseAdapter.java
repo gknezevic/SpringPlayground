1a001be8defb1c268f672fb7e2dc082493caba5c
Goran Knezevic <goran.knezevic@kolotree.com>
2023-07-02 16:41:40 +0200

    Saving and fetching

--- src/main/java/com/example/springplayground/adapter/DatabaseAdapter.java
+++ src/main/java/com/example/springplayground/adapter/DatabaseAdapter.java
@@ -3,19 +3,29 @@ package com.example.springplayground.adapter;
 import com.example.springplayground.model.Book;
 import com.example.springplayground.model.Genre;
 import com.example.springplayground.port.incoming.BooksPort;
+import com.example.springplayground.repository.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 
 public class DatabaseAdapter implements BooksPort {
 
+    private final BookRepository bookRepository;
+
+    public DatabaseAdapter(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public Book save(Book book) {
-        return null;
+        return this.bookRepository.save(book);
     }
 
     @Override
     public Book get(String id) {
-        return null;
+        return this.bookRepository
+                .findById(id)
+                .orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
--- src/test/java/com/example/springplayground/SpringPlaygroundApplicationTests.java
+++ src/test/java/com/example/springplayground/SpringPlaygroundApplicationTests.java
@@ -4,30 +4,40 @@ import com.example.springplayground.adapter.DatabaseAdapter;
 import com.example.springplayground.model.Book;
 import com.example.springplayground.model.Genre;
 import com.example.springplayground.port.incoming.BooksPort;
+import com.example.springplayground.repository.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringPlaygroundApplicationTests {
 
     private BooksPort booksPort;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @BeforeEach
     public void beforeEach() {
-        booksPort = new DatabaseAdapter();
+        booksPort = new DatabaseAdapter(bookRepository);
     }
 
     @Test
-    public void shouldReturnedStoredBookUsingIdTest() {
+    void shouldReturnedStoredBookUsingIdTest() {
         Book book = Book.create("My Test book", Genre.COMEDY);
         Book savedBook = booksPort.save(book);
         Book fetchedBook = booksPort.get(savedBook.getId());
         assertNotNull(fetchedBook);
         assertEquals(savedBook.getId(), fetchedBook.getId());
+    }
+
+    @Test
+    void shouldThrowExceptionIfBookIsNotFoundTest() {
+        assertThrows(EntityNotFoundException.class, () -> booksPort.get("XYZ"));
     }
 
 }
